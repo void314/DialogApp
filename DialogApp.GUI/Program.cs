0db2d89c3540dba2e7f7894b89493f8ebbcf85bb
Artem <71975200+void314@users.noreply.github.com>
2025-11-05 23:25:55 +0500

    feat: Реализована функция получения списка

--- DialogApp.GUI/Program.cs
+++ DialogApp.GUI/Program.cs
@@ -1,7 +1,25 @@
-﻿namespace DialogApp.GUI
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using DialogApp.BLL; // Нужен доступ к слою BLL
 
+namespace DialogApp.GUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Приложение Диалогового Помощника ---");
+
+            TableManager manager = new TableManager();
+
+            List<string> availableTables = manager.GetAllAvailableTables();
+
+            Console.WriteLine("\nДоступные столики:");
+            foreach (var table in availableTables)
+            {
+                Console.WriteLine($"- {table}");
+            }
+            Console.ReadLine();
+        }
     }
 }
